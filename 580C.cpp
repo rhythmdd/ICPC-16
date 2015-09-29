293859b0d71585118e6fe415bf0d8c90ad6061d0
rhythmdd <rdrhythm645@gmail.com>
2015-09-30 02:48:15 +0530

    Update 580C.cpp

--- 580C.cpp
+++ 580C.cpp
@@ -8,24 +8,24 @@
 using namespace std;
 long long int n,m;
 typedef struct node{
-    long long int val,visit,c;
+    long long int val;
     vector<long long int> arr;
 }node;
-stack<long long int> dd;
-node a[100000];
+//stack<long long int> dd;
+node a[100005];
 
 long long int dfs(long long int k,long long int c){
     long long int i,ctr=0;
-    a[k].visit=1;
-
-    //cout<<k<<" "<<c<<endl;
 
     if(a[k].val){
         c++;
     }
     else c=0;
+
+    cout<<k<<" "<<c<<endl;
     if(c>m)
     return 0;
+
     //stack.push(k);
     for(i=0;i<a[k].arr.size();i++){
             ctr+=dfs(a[k].arr[i],c);
@@ -33,7 +33,7 @@ long long int dfs(long long int k,long long int c){
     if(!a[k].arr.size()){
         ctr=1;
     }
-    //cout<<"ctr= "<<ctr<<endl<<endl;
+    cout<<"ctr= "<<ctr<<endl<<endl;
     return ctr;
 
 }
@@ -43,11 +43,11 @@ int main(){
     long long int i,j,k;
     cin>>n>>m;
     for(i=0;i<n;i++){
-        scanf("%d",&a[i].val);
+        scanf("%lld",&a[i].val);
     }
     for(i=0;i<n-1;i++){
-        scanf("%d %d",&j,&k);
-        (a[j-1].arr).push_back(k-1);
+        scanf("%lld %lld",&j,&k);
+        a[j-1].arr.push_back(k-1);
     }
 
     cout<<dfs(0,0)<<endl;
