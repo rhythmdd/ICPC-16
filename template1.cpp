4e184fa0af7ce8375a47f167832f11bb51653364
Rishabh Gupta <rishabh1351995@users.noreply.github.com>
2015-10-09 17:44:53 +0530

    Create template1.cpp

---
+++ template1.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+#include<cmath>
+#include<vector>
+#include<cstring>
+#include<algorithm>
+#include<iostream>
+#include <stdio.h>
+#include <math.h>
+#include <bitset>  
+using namespace std;
+#include <utility>
+
+
+// Numeric Constants
+#define mod 1000000007
+#define maxs 100005
+#define mins 1005
+#define eps 0.000000000001
+#define imax 2000000200
+#define llmax 1000000002000000000ll
+#define pi 3.141592653589793
+
+// Others
+#define LL long long int
+#define pb push_back
+#define gc getchar_unlocked
+#define iosbase ios_base::sync_with_stdio(false)
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define ppi pair<pair<int,int>,int>
+#define ppl pair<pll,ll>
+#define vi vector<int>
+#define sc scanf
+#define pr printf
+#define lld I64d
+#define F first
+#define S second
+#define siter set<int>::iterator
+#define p_pq priority_queue
+#define ub upper_bound
+#define lb lower_bound
+
+#define REP(i,a,b) for (LL i = a; i < b; ++i)
+int main(){
+	iosbase;
+	
+
+}
