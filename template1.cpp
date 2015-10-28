c1637d64ce5b6634a1b6d5529ab86da18ecdbe1b
Rishabh Gupta <rishabh1351995@users.noreply.github.com>
2015-10-28 17:29:11 +0530

    Update template1.cpp

--- template1.cpp
+++ template1.cpp
@@ -1,19 +1,37 @@
-#include<bits/stdc++.h>
-#include<cmath>
-#include<vector>
-#include<cstring>
-#include<algorithm>
-#include<iostream>
-#include <stdio.h>
-#include <math.h>
-#include <bitset>  
-using namespace std;
+#include <bits/stdc++.h>
 #include <utility>
+#include <vector> 
+#include <list> 
+#include <map> 
+#include <set> 
+#include <deque> 
+#include <queue> 
+#include <stack> 
+#include <bitset> 
+#include <algorithm> 
+#include <functional> 
+#include <numeric> 
+#include <utility> 
+#include <sstream> 
+#include <iostream> 
+#include <iomanip> 
+#include <cstdio> 
+#include <cmath> 
+#include <cstdlib> 
+#include <cctype> 
+#include <string> 
+#include <cstring> 
+#include <cstdio> 
+#include <cmath> 
+#include <cstdlib> 
+#include <ctime> 
+
+using namespace std; 
 
 
-// Numeric Constants
 #define mod 1000000007
-#define maxs 100005
+#define maxs 10005
+#define array_size_limit 5000005
 #define mins 1005
 #define eps 0.000000000001
 #define imax 2000000200
@@ -29,7 +47,7 @@ using namespace std;
 #define pll pair<ll,ll>
 #define ppi pair<pair<int,int>,int>
 #define ppl pair<pll,ll>
-#define vi vector<int>
+#define vi vector<LL>
 #define sc scanf
 #define pr printf
 #define lld I64d
@@ -39,10 +57,22 @@ using namespace std;
 #define p_pq priority_queue
 #define ub upper_bound
 #define lb lower_bound
+#define PI acos(-1)
+#define CLEAR(A) memset(A,0,sizeof(A))
+#define SETMAX(A) memset(A,0x7f,sizeof(A))
+#define SETM1(A) memset(A,-1,sizeof(A))
+#define SQ(A) (A)*(A)
+#define sqr(a) (((a) * (a)))
+#define all(a) a.begin() , a.end()
+#define error(x) cerr << #x << " = " << (x) <<endl
+#define Error(a,b) cerr<<"( "<<#a<<" , "<<#b<<" ) = ( "<<(a)<<" , "<<(b)<<" )\n";
+#define errop(a) cerr<<#a<<" = ( "<<((a).x)<<" , "<<((a).y)<<" )\n";
+#define coud(a,b) cout<<fixed << setprecision((b)) << (a)
+#define L(x) ((x)<<1)
+#define R(x) (((x)<<1)+1)
+#define uarray unordered_array
+#define double long double
+#define const1 1000005
+#define pol1 cout<<" wrong"<<"  "<<l<<"  "<<mid<<" "<< left<<"  "<<right<<endl;
+#define pol cout<<" right"<<"  "<<l<<"  "<<mid<<" "<< left<<"  "<<right<<endl;
 
-#define REP(i,a,b) for (LL i = a; i < b; ++i)
-int main(){
-	iosbase;
-	
-
-}
